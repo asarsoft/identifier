2aa013cf761b2168c7109e0faad1f5eda4de21b7
agilasadi <newsma.95@gmail.com>
2019-06-21 22:32:19 +0300

    text editor value field was added

--- resources/views/admin_views/components/text_editor.blade.php
+++ resources/views/admin_views/components/text_editor.blade.php
@@ -1,22 +1,20 @@
 @section('style')
-    <link href="{{ asset('application_css/external/summernote/summernote-bs4.css') }}" rel="stylesheet">
+	<link href="{{ asset('application_css/external/summernote/summernote-bs4.css') }}" rel="stylesheet">
 @endsection
 
 <div class="mb-4">
-    <label for="{{ $field_name }}">{{ trans('button_input.'.$field_name) }}</label>
-    <textarea id="{{ $field_name }}" name="{{ $field_name }}"></textarea>
+	<label for="{{ $field_name }}">{{ trans('button_input.'.$field_name) }}</label>
+	<textarea id="{{ $field_name }}" name="{{ $field_name }}">{{@$value}}</textarea>
 </div>
 
 @section('script')
-
-    <script src="{{ asset('application_js/external/summernote-bs4.js') }}"></script>
-    <script>
+	<script src="{{ asset('application_js/external/summernote-bs4.js') }}"></script>
+	<script>
         var field_name = '{{ $field_name }}';
         $('#' + field_name).summernote({
             placeholder: '{{ trans('button_input.'.@$field_name) }}',
-            value: '{{ @$value }}',
             tabsize: 2,
             height: 200
         });
-    </script>
+	</script>
 @endsection
