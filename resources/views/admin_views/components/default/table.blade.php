97701ba22f7e05a3860457d67bf12b7e15f95591
agilasadi <newsma.95@gmail.com>
2019-06-30 20:08:48 +0300

    Show button was added

--- resources/views/admin_views/components/default/table.blade.php
+++ resources/views/admin_views/components/default/table.blade.php
@@ -9,7 +9,7 @@
 			@endif
 		@endforeach
 			<th>
-				{{ trans('button_input.actions') }}
+				<i class="fas fa-toolbox mr-2"></i>{{ trans('button_input.actions') }}
 			</th>
 	</tr>
 	</thead>
@@ -32,6 +32,11 @@
 				@endif
 			@endforeach
 			<td>
+				<a href="{{ route('show-'.$model, $record['id']) }}"
+				   class="btn mr-2 btn-sm btn-outline-info btn-lg"
+				   role="button" aria-pressed="true">
+					<i class="fas fa-clipboard-list"></i>
+				</a>
 				<a href="{{ route('edit-'.$model, $record['id']) }}"
 				   class="btn mr-2 btn-sm btn-outline-primary btn-lg"
 				   role="button" aria-pressed="true">
