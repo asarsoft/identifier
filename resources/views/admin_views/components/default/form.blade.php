263be3793fdd0c11b0e122036781c7d775000ec8
agilasadi <newsma.95@gmail.com>
2019-07-08 17:45:21 +0300

    BelongsTo insert component was created

--- resources/views/admin_views/components/default/form.blade.php
+++ resources/views/admin_views/components/default/form.blade.php
@@ -1,9 +1,9 @@
 <form class="mb-4" method="post" action="{{ route('store-'.$model, true) }}" enctype="multipart/form-data">
 	@csrf
 
-	@foreach($fields as $field)
-		@if(in_array($method, $field['available_in'], true))
-			@component('admin_views.components.default.insert.'.$field['type'], ['field' => $field])
+	@foreach($fields as $field_key => $field_value)
+		@if(in_array($method, $field_value['available_in'], true))
+			@component('admin_views.components.default.insert.'.$field_value['type'], ['field' => $field_value, 'key' => $field_key])
 			@endcomponent
 		@endif
 	@endforeach
--- resources/views/admin_views/components/default/insert/belongsTo.blade.php
+++ resources/views/admin_views/components/default/insert/belongsTo.blade.php
@@ -1,1 +1,11 @@
+<label for="{{ $key }}">
+	{{ trans('button_input.'.$key) }}
+</label>
+
+<select class="form-control" name="{{ $key }}" id="{{ $key }}">
+	@foreach($field['data'] as $option)
+		<option value="{{ $option["id"] }}">{{ $option[$field['title']] }}</option>
+	@endforeach
+</select>
+
 {{ dd($field) }}
